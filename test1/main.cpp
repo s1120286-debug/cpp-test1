136c6ad94a33d49fb5c01491abe9e5509429030c
s1120286-debug <s1120286@dmhs.kh.edu.tw>
2025-09-18 07:07:32 +0000

    轉換溫度

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+using namespace std;
+// 華氏攝氏轉換
+
 int main() {
-  const double PI = 3.141592653589793; // const：不可被修改的常數
-  int r = 3;                           // 半徑
-  double area = PI * r * r;            // 自動提升型別：int r 會提升為 double 再運算
-  std::cout << "circle area=" << area << "\n";
-  // PI = 3.14; // ⚠️ 解鎖這行會編譯錯：const 不能被重新指派
-  return 0;
+    double fahrenheit;
+    double celsius;
+    cout << "請輸入華氏溫度:\n";
+    cin >> fahrenheit;
+    celsius = (fahrenheit - 32) / 9 * 5;
+    cout << "攝氏溫度是:" << celsius;
+    return 0;
 }
